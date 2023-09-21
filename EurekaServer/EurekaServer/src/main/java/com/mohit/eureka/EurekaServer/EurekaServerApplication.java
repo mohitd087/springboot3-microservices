3640a32859b917cd736019b4d4bb0d8aaec72d01
Mohit Dubey <mohitd087@gmail.com>
2023-09-21 12:15:55 +0530

    Adding Eureka naming server and using it to retrieve url of currencyExchange MS instead of hardcoded url.

---
+++ EurekaServer/EurekaServer/src/main/java/com/mohit/eureka/EurekaServer/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.mohit.eureka.EurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
