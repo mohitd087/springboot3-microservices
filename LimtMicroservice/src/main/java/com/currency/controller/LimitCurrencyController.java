f95fadf3508e7665a78aa694a05d1942f3d84f91
Mohit Dubey <mohitd087@gmail.com>
2023-09-23 21:12:37 +0530

    Working on Retry,RateLimiter,CircuitBreaker API of resiliencej

--- LimtMicroservice/src/main/java/com/currency/controller/LimitCurrencyController.java
+++ LimtMicroservice/src/main/java/com/currency/controller/LimitCurrencyController.java
@@ -3,17 +3,22 @@ package com.currency.controller;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import com.currency.dao.CurrencyExchangeDao;
 import com.currency.lib.CurrencyParameters;
+import ch.qos.logback.classic.Logger;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 public class LimitCurrencyController {
@@ -25,10 +30,15 @@ public class LimitCurrencyController {
 
 	@Autowired
 	CurrencyExchangeDao exchangeDao;
+	private Logger logger= (Logger) LoggerFactory.getLogger(LimitCurrencyController.class);
 
 	@GetMapping("limitService/{from}/currency/{to}/{quantity}")
+//	@Retry(name="default",fallbackMethod = "defaultReturn")
+	@RateLimiter(name="default",fallbackMethod="defaultReturn")
+//	@CircuitBreaker(name="default",fallbackMethod = "defaultReturn")
 	public ResponseEntity<CurrencyParameters> limitService(@PathVariable String from, @PathVariable String to,
 			@PathVariable Integer quantity) {
+		logger.info("Limit Service called : ");
 		HashMap<String, String> idParameter = new HashMap<>();
 		idParameter.put("from", from);
 		idParameter.put("to", to);
@@ -38,6 +48,11 @@ public class LimitCurrencyController {
 				new BigDecimal(quantity).multiply(new BigDecimal(result.getConversion_amount())),
 				result.getPort()), HttpStatus.OK);
 
+	}
+	
+	public ResponseEntity<String> defaultReturn(String a,String b,Integer c,Exception e) {
+		return new ResponseEntity<String>(a + " " + b + " " + c + " "  +"Fallback-return",HttpStatus.INTERNAL_SERVER_ERROR);
+	//http://localhost:8080/limitService/INR/currency/USD/100 Returned this value	"INR USD 100 Fallback-return"
 	}
 
 }
