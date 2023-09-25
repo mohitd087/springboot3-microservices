0b82a3e61edcb176e3cf533630daeb476e93bfe0
Mohit Dubey <mohitd087@gmail.com>
2023-09-25 13:12:13 +0530

    Adding metrics and distributed log tracing using bravo implemention of Micrometric logs tracing specification.

--- LimtMicroservice/src/main/java/com/mohit/demo/LimtMicroservice/LimtMicroserviceApplication.java
+++ LimtMicroservice/src/main/java/com/mohit/demo/LimtMicroservice/LimtMicroserviceApplication.java
@@ -4,9 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import feign.RequestInterceptor;
+import io.micrometer.tracing.Tracer;
 @SpringBootApplication
 @EnableFeignClients("com.currency.controller")
 @EnableJpaRepositories("com.currency.dao")
@@ -17,5 +20,14 @@ public class LimtMicroserviceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LimtMicroserviceApplication.class, args);
 	}
+	
+
+	    @Bean
+	    public RequestInterceptor braveRequestInterceptor(Tracer tracer) {
+	        return template -> {
+	                template.header("x-b3-traceid" , tracer.currentSpan().context().traceId());
+	                template.header("x-b3-spanid", tracer.currentSpan().context().spanId());
+	        };
+	    }
 
 }
